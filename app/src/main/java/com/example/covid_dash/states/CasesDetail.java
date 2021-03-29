31911a7a2186ce4797d29b45494b55d338876028
juhichaudhari <juhichaudhari@gmail.com>
2021-03-28 23:47:16 -0400

    cases detail now have access to the details

--- app/src/main/java/com/example/covid_dash/states/CasesDetail.java
+++ app/src/main/java/com/example/covid_dash/states/CasesDetail.java
@@ -4,10 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.covid_dash.R;
+
+import org.parceler.Parcels;
+
 public class CasesDetail extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        StateDetailGetter details = (StateDetailGetter) Parcels.unwrap(getIntent().getParcelableExtra("details"));
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cases_detail);
     }
