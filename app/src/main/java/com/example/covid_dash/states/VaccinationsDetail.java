8a3e2cb9df370cecc40f9791e3d6f2c3e1088859
juhichaudhari <juhichaudhari@gmail.com>
2021-03-30 11:54:41 -0400

    displaying vaccination information

--- app/src/main/java/com/example/covid_dash/states/VaccinationsDetail.java
+++ app/src/main/java/com/example/covid_dash/states/VaccinationsDetail.java
@@ -3,14 +3,34 @@ package com.example.covid_dash.states;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.example.covid_dash.R;
 
+import org.parceler.Parcels;
+
 public class VaccinationsDetail extends AppCompatActivity {
+
+    TextView tvVaccinationsInitiated;
+    TextView tvVaccinationsDistributed;
+    TextView tvVaccinationsCompleted;
+    TextView tvVaccinationsAdministered;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        StateDetailGetter details = (StateDetailGetter) Parcels.unwrap(getIntent().getParcelableExtra("details"));
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_vaccinations_detail);
+
+        tvVaccinationsInitiated = findViewById(R.id.tvVaccinationsInitiated);
+        tvVaccinationsDistributed = findViewById(R.id.tvVaccinationsDistributed);
+        tvVaccinationsCompleted = findViewById(R.id.tvVaccinationsCompleted);
+        tvVaccinationsAdministered = findViewById(R.id.tvVaccinationsAdministered);
+
+        tvVaccinationsInitiated.setText("Vaccines\nInitiated\n" + details.getVaccinationsInitiated());
+        tvVaccinationsDistributed.setText("Vaccines\nDistributed\n" + details.getVaccinesDistributed());
+        tvVaccinationsCompleted.setText("Vaccines\nCompleted\n" + details.getVaccinationsCompleted());
+        tvVaccinationsAdministered.setText("Vaccines\nAdministered\n" + details.getVaccinesAdministered());
     }
 }
