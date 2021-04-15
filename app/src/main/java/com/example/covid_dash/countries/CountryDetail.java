58419b5b3c440bffd04cf4ee80347c7c12ab4fae
Akash Patel <ap.akash.patel1999+github@gmail.com>
2021-04-15 17:08:15 -0400

    getting the data for countries

--- app/src/main/java/com/example/covid_dash/countries/CountryDetail.java
+++ app/src/main/java/com/example/covid_dash/countries/CountryDetail.java
@@ -1,14 +1,16 @@
-package com.example.covid_dash;
+package com.example.covid_dash.countries;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class SearchCountry extends AppCompatActivity {
+import com.example.covid_dash.R;
+
+public class CountryDetail extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_search_country);
+        setContentView(R.layout.activity_country_detail);
     }
 }
